2481fc3853549b996ae6304fb7463b532fdc43e5
Sai Reddy <sai6221@gmail.com>
2021-07-28 16:34:34 +0530

    Complete Dicee app

--- RollDice/ViewController.swift
+++ RollDice/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImage1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImage2.image = #imageLiteral(resourceName: "DiceSix")
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let dices =  [ #imageLiteral(resourceName: "DiceOne") ,#imageLiteral(resourceName: "DiceTwo") , #imageLiteral(resourceName: "DiceThree") ,#imageLiteral(resourceName: "DiceFour") ,#imageLiteral(resourceName: "DiceFive") ,#imageLiteral(resourceName: "DiceSix") ]
+        
+        diceImage1.image = dices[Int.random(in: 0...5)]
+        diceImage2.image = dices[Int.random(in: 0...5)]
+        
+    
+    }
+    
 }
 
